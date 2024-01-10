437fa1f702db563ea89f4aacec84c26b0770a121
蘇暐倫 <s711261130@gm.ntpu.edu.tw>
2024-01-10 19:37:04 +0800

    new

---
+++ Olg_model/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Olgmoder',
+    version='1.0.0',
+    description='Description of your package',
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=['dependency1', 'dependency2'],
+)
