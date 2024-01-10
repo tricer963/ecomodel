463d786d91c7e6aebb3455d7be3902f97ae16e25
蘇暐倫 <s711261130@gm.ntpu.edu.tw>
2024-01-10 19:54:12 +0800

    v1.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Olg_model',
+    name='ecomodel',
     version='1.0.0',
     description='Description of your package',
     author='Your Name',
