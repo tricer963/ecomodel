ed98cbf4657db48fc9b7b3871fb6379c23a3d2c2
蘇暐倫 <s711261130@gm.ntpu.edu.tw>
2024-01-10 20:03:49 +0800

    v1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ecomodel',
-    version='1.0.0',
+    version='1.0.4',
     description='Description of your package',
     author='Your Name',
     packages=find_packages(),
